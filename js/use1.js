c1c2aba3e882837014000851bd39e9b64fa09705
yfwl-pm001 <yfwl-pm001@yfwl-UI02-Air.local>
2020-05-20 20:22:21 +0800

    1

--- js/use1.js
+++ js/use1.js
@@ -1,14 +1,14 @@
 $(function () {
-    TweenMax.from($('.bgBox~*'), 4, { scale: 5, opacity: 0, delay: 0 });
-    TweenMax.from($('.bgBox'), 4, { scale: 1, opacity: 0, delay: 0 });
-    TweenMax.to($('.bgBox'), 4, { scale: 1, opacity: 1, delay: 0 });
-    TweenMax.from($('.theNum'), 4, { scale: 4, y: "-400%", x: "10%", opacity: 0, ease: Power3.easeInOut, delay: 0 });
-    TweenMax.to($('.theNum'), 4, { opacity: 1, ease: Power3.easeInOut, delay: 0 });
-    TweenMax.from($('.theRefer'), 4, { scale: 4, y: "-380%", x: "-10%", opacity: 0, ease: Power3.easeOut, delay: 2.8 });
-    TweenMax.to($('.theRefer'), 4, { opacity: 1, ease: Power3.easeOut, delay: 2.8 });
-    TweenMax.from($('.theTitle'), 4.5, { scale: 5, y: "0%", x: "10%", opacity: 0, ease: Power3.easeOut, delay: 4 });
-    TweenMax.to($('.theTitle'), 4.5, { opacity: 1, ease: Power3.easeOut, delay: 4 });
-    TweenMax.from($('.theScarf'), 6, { scale: 5, y:"200%",x:"0", opacity:0,ease: Power2.easeOut,delay:5 });
-    TweenMax.to($('.theScarf'), 6, { opacity:1, ease: Power2.easeOut,delay:5 });
-    TweenMax.to($('.bgBox~*'), 3, { scale: 1, opacity: 1, delay: 8 });
+    TweenMax.from($('.bgBox~*'), 3, { scale: 5, opacity: 0, delay: 0 });
+    TweenMax.from($('.bgBox'), 3, { scale: 1, opacity: 0, delay: 0 });
+    TweenMax.to($('.bgBox'), 3, { scale: 1, opacity: 1, delay: 0 });
+    TweenMax.from($('.theNum'), 3, { scale: 4, y: "-400%", x: "10%", opacity: 0, ease: Power3.easeInOut, delay: 0 });
+    TweenMax.to($('.theNum'), 3, { opacity: 1, ease: Power3.easeInOut, delay: 0 });
+    TweenMax.from($('.theRefer'), 3, { scale: 4, y: "-380%", x: "-10%", opacity: 0, ease: Power3.easeOut, delay: 2 });
+    TweenMax.to($('.theRefer'), 3, { opacity: 1, ease: Power3.easeOut, delay: 2 });
+    TweenMax.from($('.theTitle'), 3.5, { scale: 5, y: "0%", x: "10%", opacity: 0, ease: Power3.easeOut, delay: 3 });
+    TweenMax.to($('.theTitle'), 3.5, { opacity: 1, ease: Power3.easeOut, delay: 3 });
+    TweenMax.from($('.theScarf'), 4.2, { scale: 5, y:"200%",x:"0", opacity:0,ease: Power2.easeOut,delay:4 });
+    TweenMax.to($('.theScarf'), 4.2, { opacity:1, ease: Power2.easeOut,delay:4 });
+    TweenMax.to($('.bgBox~*'), 3, { scale: 1, opacity: 1, delay: 6 });
 })
